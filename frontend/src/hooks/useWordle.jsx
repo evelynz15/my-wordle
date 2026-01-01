656d8025368e78476157249f745a9dd639a73b73
Evelyn Zhu <evelynzhu@Evelyns-MacBook-Air.local>
2026-01-01 23:35:38 +0100

    new wordle project, only basic functionality

---
+++ frontend/src/hooks/useWordle.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react"
+
+const useWordle = (solution) => {
+    const [turn, setTurn] = useState(0)
+    const [currentGuess, setCurrentGuess] = useState('')
+    const [guesses, setGuesses] = useState([]) // each guess is an array
+    const [history, setHistory] = useState([]) // each guess is a string
+    const [isCorrect, setIsCorrect] = useState(false)
+
+    // format a guess into an array of letter objects
+    // e.g. [{key: 'a', colour: 'yellow'}]
+    const formatGuess = () => {
+        let solutionArray = [...solution]
+        let formattedGuess = [...currentGuess].map((l) => {
+            return {key: l, colour: 'grey'}
+        })
+
+        // find any green letters
+        formattedGuess.forEach((l, i) => {
+            if (solutionArray[i] === l.key) {
+                formattedGuess[i].colour = 'green'
+                solutionArray[i] = null
+            }
+        })
+
+        // find any yellow colours
+        formattedGuess.forEach((l, i) => {
+            if (solutionArray.includes(l.key) && l.colour !== 'green') {
+                formattedGuess[i].colour = 'yellow'
+                solutionArray[solutionArray.indexOf(l.key)] = null
+            }
+        })
+
+        return formattedGuess
+    }
+
+    // add a new guess to the guesses state
+    // update the isCorrect state if guess is correct
+    // add one to the turn state
+    const addNewGuess = () => {
+
+    }
+
+    // handle keyup event & track current guess
+    // if user presses enter, add the new guess
+    const handleKeyup = ({ key }) => {
+        
+        if (key === 'Enter') {
+            // only add guess if turn is less than 5
+            if (turn > 5) {
+                console.log('You used all your guesses')
+                return
+            }
+            // do not allow duplicate words
+            if (history.includes(currentGuess)) {
+                console.log('You already tried that word')
+                return
+            }
+            // check word is 5 chars long
+            if (currentGuess.length != 5) {
+                console.log('Word must be 5 characters long')
+                return
+            }
+
+            const formatted = formatGuess()
+            console.log(formatted)
+        }
+
+        if (key === 'Backspace') {
+            setCurrentGuess((prev) => {
+                return prev.slice(0, -1)
+            })
+            return
+        }
+        
+        if (/^[A-Za-z]$/.test(key)) {
+            if(currentGuess.length < 5) {
+                setCurrentGuess((prev) => {
+                    return prev + key
+                })
+            }
+        }
+    }
+
+    return {turn, currentGuess, guesses, isCorrect, handleKeyup}
+
+}
+
+export default useWordle
