f6114c6cc4d1d20cc93d7de68b6d8e3290a03d17
Evelyn Zhu <evelynzhu@Evelyns-MacBook-Air.local>
2026-01-04 22:55:37 +0100

    created grid

--- frontend/src/hooks/useWordle.jsx
+++ frontend/src/hooks/useWordle.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 const useWordle = (solution) => {
     const [turn, setTurn] = useState(0)
     const [currentGuess, setCurrentGuess] = useState('')
-    const [guesses, setGuesses] = useState([]) // each guess is an array
+    const [guesses, setGuesses] = useState([...Array(6)]) // each guess is an array
     const [history, setHistory] = useState([]) // each guess is a string
     const [isCorrect, setIsCorrect] = useState(false)
 
@@ -37,8 +37,22 @@ const useWordle = (solution) => {
     // add a new guess to the guesses state
     // update the isCorrect state if guess is correct
     // add one to the turn state
-    const addNewGuess = () => {
-
+    const addNewGuess = (formattedGuess) => {
+        if (currentGuess === solution) {
+            setIsCorrect(true)
+        }
+        setGuesses((prevGuesses) => {
+            let newGuesses = [...prevGuesses]
+            newGuesses[turn] = formattedGuess
+            return newGuesses
+        })
+        setHistory((prevHistory) => {
+            return [...prevHistory, currentGuess]
+        })
+        setTurn((prevTurn) => {
+            return prevTurn + 1
+        })
+        setCurrentGuess('')
     }
 
     // handle keyup event & track current guess
@@ -63,7 +77,7 @@ const useWordle = (solution) => {
             }
 
             const formatted = formatGuess()
-            console.log(formatted)
+            addNewGuess(formatted)
         }
 
         if (key === 'Backspace') {
